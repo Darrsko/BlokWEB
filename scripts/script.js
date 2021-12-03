4161740e43f1dd246b76969ed86039f3f954cff8
Darrsko <94625391+Darrsko@users.noreply.github.com>
2021-12-03 15:04:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
-// JavaScript Document
-console.log("hello world!");
+
+// ADD CLASS ON SCROLL
+let scrollpos = window.scrollY;
+const header = document.querySelector("header nav");
+const image_height = document.querySelector("main > section:first-of-type").offsetHeight;
+
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY;
+
+// ALS MEER DAN SECTION 1 POSITIE IS GESCROLLED, GEEF CLASS MEE
+  if (scrollpos >= image_height) {
+    header.classList.add("scrolled");
+  }
+  else {
+      header.classList.remove("scrolled");
+  }
+
+})
